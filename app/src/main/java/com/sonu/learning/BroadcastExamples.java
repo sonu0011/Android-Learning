9950c08504dc2bcf8e9f1faddd8bc1b78d090db6
Sonu kumar <sonu1633900@gmail.com>
2021-07-16 17:25:47 +0530

    br continue ...

--- app/src/main/java/com/sonu/learning/BroadcastExamples.java
+++ app/src/main/java/com/sonu/learning/BroadcastExamples.java
@@ -2,13 +2,33 @@ package com.sonu.learning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 
+import com.sonu.learning.br.MyBroadcastReceiver;
+
 public class BroadcastExamples extends AppCompatActivity {
+//    MyBroadcastReceiver receiver = new MyBroadcastReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_broadcast_examples);
+//        registerReceiver(receiver , new IntentFilter("com.sonu.EXAMPLE_BROADCAST"));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        //registerReceiver(receiver, intentFilter);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+//        unregisterReceiver(receiver);
     }
 }
--- app/src/main/java/com/sonu/learning/br/MyBroadcastReceiver.java
+++ app/src/main/java/com/sonu/learning/br/MyBroadcastReceiver.java
@@ -4,15 +4,37 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.sonu.learning.BroadcastExamples;
+
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            Toast.makeText(context, "Boot completed", Toast.LENGTH_SHORT).show();
-        } else if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
-            Toast.makeText(context, "Conectivity change", Toast.LENGTH_SHORT).show();
-        }
+        //intent.getStringExtra("com.sonu.EXMPLE_BROADCAST_EXTRA");
+        Log.d("My broadcast receiver", "onReceive: " + intent.getAction());
+        context.startActivity(new Intent(context, BroadcastExamples.class).setFlags(FLAG_ACTIVITY_NEW_TASK));
+//        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+//            Toast.makeText(context, "Boot completed", Toast.LENGTH_SHORT).show();
+//        } else
+//        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
+//            Toast.makeText(context, "Conectivity change", Toast.LENGTH_SHORT).show();
+//            boolean extra = intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+//
+//            if (extra) {
+//                Toast.makeText(context, "No internet ", Toast.LENGTH_SHORT).show();
+//            } else {
+//                Toast.makeText(context, "Connected to internet", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+
+//        if ("com.sonu.EXAMPLE_BROADCAST".equals(intent.getAction())) {
+//            Toast.makeText(context, "Custom Broadcast received", Toast.LENGTH_SHORT).show();
+//        }
+
+        Toast.makeText(context, "Broadcas received", Toast.LENGTH_SHORT).show();
     }
 }
